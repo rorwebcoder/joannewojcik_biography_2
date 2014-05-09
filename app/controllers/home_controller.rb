0002f0a26e50e361453798af78cb30c7c8a48951
udhaya <ror.webcoder@gmail.com>
2014-05-08 23:50:07 -0700

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+		def show
+		end
+		
+		def about
+		end
+		
+		def contact
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoanneWojcik2::Application.config.secret_token = '0e816c6ba486b22a2b68e4a1b6970981637f0564e530d95f469d5193a69cdfc0bd6ebc2486dc6213a82804ab025b1bdb4f94c7f85f636faf3af425bbc7cf3a05'
